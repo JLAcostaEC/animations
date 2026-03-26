af11c2e51b92f0ab7b31ce25a0ad2843f09b2f66
SikandarJODD <sikandar6174@gmail.com>
2026-03-26 16:12:17 +0530

    feat: base new badge and qrcode component

--- src/routes/spell/+page.svelte
+++ src/routes/spell/+page.svelte
@@ -1,50 +1,15 @@
 <script lang="ts">
 	import { PerspectiveBook } from "$lib/components/spell/perspective-book";
+	import { QRCode } from "$lib/components/spell/qrcode";
 	import { Code2, Cog } from "@lucide/svelte";
 
 	const textureUrl = "/image.avif";
 </script>
 
 <div class="flex min-h-screen flex-col items-center justify-center gap-6">
-	<PerspectiveBook textured {textureUrl} size="lg">
-		<div class="flex h-full flex-col justify-between">
-			<div class="flex flex-col gap-4">
-				<div class="flex items-start justify-between gap-4">
-					<h2 class="max-w-[12ch] text-lg leading-5 font-semibold sm:text-xl">
-						Building Interfaces the Svelte Way
-					</h2>
-					<Code2 class="mt-0.5 size-6 shrink-0" />
-				</div>
-
-				<p
-					class="max-w-[18ch] text-xs/relaxed font-medium text-white/80 sm:text-sm/relaxed"
-				>
-					A compact tribute to compiler-driven UI design and the creator behind it.
-				</p>
-			</div>
-
-			<div class="space-y-1">
-				<p class="text-[0.65rem] font-semibold tracking-[0.28em] text-white/70 uppercase">
-					Creator of Svelte
-				</p>
-				<span class="font-serif text-base font-medium sm:text-lg">Rich Harris</span>
-			</div>
-		</div>
-	</PerspectiveBook>
-
-	<PerspectiveBook
-		class="h-full justify-start bg-[#4D4E41] text-yellow-300"
-		textured
-		{textureUrl}
+	<div
+		class="w-[105px] md:w-[140px] rounded-lg p-2 shadow-[0_0_0_1px_rgba(0,0,0,.08),_0px_2px_2px_rgba(0,0,0,.04)] dark:border [&_svg]:w-full [&_svg]:h-auto"
 	>
-		<div class="flex h-full flex-col justify-between">
-			<div class="flex flex-col gap-4">
-				<h2 class="leading-4 font-semibold">The Art of Doing Science and Engineering</h2>
-				<Cog class="size-5" />
-			</div>
-			<span class="font-serif text-sm font-medium">
-				Richard<br />W. Hamming
-			</span>
-		</div>
-	</PerspectiveBook>
+		<QRCode value="https://spell.sh" size={140} />
+	</div>
 </div>
